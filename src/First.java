97d7ae9297933554684cc5cc6a843757fbd3db53
RinkiLodhi <rinki.lanos@gmail.com>
2025-08-31 13:30:29 +0530

    meri first file

--- src/First.java
+++ src/First.java
@@ -1,5 +1,7 @@
 public class First {
     public static void main(String[] args) {
         System.out.println("this is first class of spring");
+        System.out.println("rinki is a briliant software developer");
+        System.out.println("first developer 7978");
     }
 }
